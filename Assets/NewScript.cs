dcbac484652b6135d1a47e663c18f02a92fbcdb4
hamadqatham <hamad.qatham@gmail.com>
2019-08-08 14:02:15 +0300

    added a new script

---
+++ Assets/NewScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("TEST");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
